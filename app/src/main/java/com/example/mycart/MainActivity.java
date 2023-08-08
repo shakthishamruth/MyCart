d45612d9ac876e83f4883fc2a8ff631f73407e87
shakthishamruth <shakthishamruth@gmail.com>
2023-08-08 21:16:21 +0530

    Update Startup

--- app/src/main/java/com/example/mycart/MainActivity.java
+++ app/src/main/java/com/example/mycart/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.mycart;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startupLogIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup);
+
+        startupLogIn = findViewById(R.id.startupLogIn);
+
+    }
+
+    public void onClickLogIn(View view) {
         setContentView(R.layout.login);
     }
 }
